a6960abb65b06810f5e79df62dbb4bf7ef495e3e
netstermonir <monirhossain6163@gmail.com>
2024-01-25 00:42:29 +0600

    live data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export const revalidate = 0;
 async function getStudentData() {
   try {
-    const res = await fetch("/api/v1/user", {
+    const res = await fetch(`${process.env.VERCEL_URL}api/v1/user`, {
       cache: "no-store",
     });
     const data = await res.json();
