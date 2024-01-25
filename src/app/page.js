83f40dbc6d94448a0300d3658ceb137c548dd960
netstermonir <monirhossain6163@gmail.com>
2024-01-25 12:19:49 +0600

    v4

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
 import StudentList from "@/components/StudentLists/StudentList";
+import prisma from "@/libs/Prisma";
 import { unstable_cache } from "next/cache";
 async function getStudentData() {
-  try {
-    const res = await fetch(`${process.env.VERCEL_URL}/user`, {
-      cache: "no-store",
-    });
-    const data = await res.json();
-    return data["data"];
-  } catch (e) {
-    console.log("e");
-  }
+  // try {
+  //   const res = await fetch(`${process.env.VERCEL_URL}/user`, {
+  //     cache: "no-store",
+  //   });
+  //   const data = await res.json();
+  //   return data["data"];
+  // } catch (e) {
+  //   console.log("e");
+  // }
+  const res = await prisma.User.findMany({
+    orderBy: {
+      id: "desc",
+    },
+  });
+  return res;
 }
 export default async function Home() {
   unstable_cache(getStudentData);
