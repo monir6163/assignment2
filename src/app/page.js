54abb211560fc04412a3e26eed1a7c8ed58113c0
netstermonir <monirhossain6163@gmail.com>
2024-01-25 12:46:18 +0600

    v5

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,16 @@
-import StudentList from "@/components/StudentLists/StudentList";
-import prisma from "@/libs/Prisma";
-import { unstable_cache } from "next/cache";
 async function getStudentData() {
-  // try {
-  //   const res = await fetch(`${process.env.VERCEL_URL}/user`, {
-  //     cache: "no-store",
-  //   });
-  //   const data = await res.json();
-  //   return data["data"];
-  // } catch (e) {
-  //   console.log("e");
-  // }
-  const res = await prisma.User.findMany({
-    orderBy: {
-      id: "desc",
-    },
-  });
-  return res;
+  try {
+    const res = await fetch(`${process.env.VERCEL_URL}/user`, {
+      cache: "no-store",
+    });
+    const data = await res.json();
+    return data["data"];
+  } catch (e) {
+    console.log("e");
+  }
 }
+import StudentList from "@/components/StudentLists/StudentList";
 export default async function Home() {
-  unstable_cache(getStudentData);
   const studentsData = await getStudentData();
   return (
     <main className="flex min-h-screen flex-col px-2">
